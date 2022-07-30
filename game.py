81e44006951a2c6f90f66e85c7998bab7831549b
Siiha <90766306+Siiha@users.noreply.github.com>
2022-07-30 15:03:39 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 30 14:14:09 2022
+
+@author: siina
+"""
+
+import random
+f = open("hirsipuu/kotus_sanat.txt","r")
+r = [i[:-1] for i in f.readlines()]
+l = tuple(r[random.randint(0, len(r))])
+s = ""
+y = ["_" for i in l]
+p = 0
+while tuple(y)!=l and p < 5:
+    s+=input("Anna kirjain. aiemmin arvatut: "+s)
+    for i in range(len(l)):
+        if l[i] in s:
+            y[i] = l[i]
+    if s[-1] not in l:
+        p += 1
+    print("".join(y))
+    print(p)
+if p == 5:
+    print("hävisit")
